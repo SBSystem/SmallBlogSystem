2b583b08a6beda488ff48e159df25d3bc4b693df
Mariusz08 <nowygracz01@gmail.com>
2018-02-23 19:50:38 +0100

    Create LogLevel class.

--- src/SmallBlogSystem/Managers/LogManager.php
+++ src/SmallBlogSystem/Managers/LogManager.php
@@ -2,6 +2,8 @@
 
 namespace App\SmallBlogSystem\Managers;
 
+use App\SmallBlogSystem\Level\LogLevel;
+
 class LogManager extends BasicManager
 {
     private $initialized;
@@ -13,17 +15,29 @@ class LogManager extends BasicManager
             $this->init();
         }
     }
-    public  function getManager()
+    public  function getManager(): boolean
     {
         return $this->initialized;
     }
-    protected function init()
+    protected function init(): boolean
     {
         if(file_exists('log.txt')) {
             $this->initialized = true;
+            return true;
         } else {
             $this->initialized = false;
+            return false;
         }
 
+    }
+    public function log(LogLevel $priority, $logValue): boolean
+    {
+        if($this->initialized === true) {
+            $log = '['.$priority.'] '.$logValue;
+            file_put_contents('log.txt', $log);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
