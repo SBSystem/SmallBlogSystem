57e63529d99cf349d5e51abef42941673660494a
Mariusz08 <nowygracz01@gmail.com>
2018-03-09 19:33:35 +0100

    Added database exception

---
+++ src/SmallBlogSystem/Exception/DatabaseDoNotExistsException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\SmallBlogSystem\Exception;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class DatabaseDoNotExistsException extends Exception
+{
+    public function __construct($messageText)
+    {
+        parent::__construct('DatabaseDoNotExistsException: No database found '.$messageText);
+    }
+}
