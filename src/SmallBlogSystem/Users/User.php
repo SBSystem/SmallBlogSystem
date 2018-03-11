591517f33a9b2c5fa38ba002e83aa968ae27e283
Mariusz08 <nowygracz01@gmail.com>
2018-03-11 11:36:05 +0100

    Added User class.

---
+++ src/SmallBlogSystem/Users/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\SmallBlogSystem\Users;
+
+/**
+ * Class User
+ * @package App\SmallBlogSystem\Users
+ */
+class User
+{
+    /**
+     * @var String
+     */
+    private $username;
+    /**
+     * @var String
+     */
+    private $role;
+
+    /**
+     * User constructor.
+     * @param String $username
+     * @param String $role
+     */
+    public function __construct(String $username, String $role): bool
+    {
+        $this->username = $username;
+        $this->role = $role;
+        return true;
+    }
+
+    /**
+     * @return String
+     */
+    public function getUsername(): String
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param String $username
+     * @return bool
+     */
+    public function setUsername(String $username): bool
+    {
+        $this->username = $username;
+        return true;
+    }
+
+    /**
+     * @return String
+     */
+    public function getRole(): String
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param String $role
+     * @return bool
+     */
+    public function setRole(String $role): bool
+    {
+        $this->role = $role;
+        return true;
+    }
+}
