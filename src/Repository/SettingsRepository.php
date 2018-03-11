fec8e64e471148664eeb8265b2c346d34282f8e5
Mariusz08 <nowygracz01@gmail.com>
2018-03-11 11:03:47 +0100

    Added Settings entity

---
+++ src/Repository/SettingsRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Settings;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Settings|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Settings|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Settings[]    findAll()
+ * @method Settings[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SettingsRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Settings::class);
+    }
+
+    /*
+    public function findBySomething($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->where('s.something = :value')->setParameter('value', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+}
