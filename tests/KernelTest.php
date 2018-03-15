7c5a17229542384ae51b6ba0d13019c892831a0b
Mariusz08 <nowygracz01@gmail.com>
2018-03-15 17:40:00 +0100

    Change Asseration type

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -8,18 +8,18 @@ class KernelTest extends PHPUnit_Framework_TestCase
     {
         $kernel = new Kernel(\App\SmallBlogSystem\Level\KernelStartInfo::BOOT);
         $setShutdownStatus = $kernel->changeSystemStatus(\App\SmallBlogSystem\Level\KernelStartInfo::SHUTDOWN);
-        $this->assertEquals(true, $setShutdownStatus);
+        $this->assertTrue($setShutdownStatus);
     }
     public function testKernelSystemRebootStatus()
     {
         $kernel = new Kernel(\App\SmallBlogSystem\Level\KernelStartInfo::BOOT);
         $setRebootStatus = $kernel->changeSystemStatus(\App\SmallBlogSystem\Level\KernelStartInfo::REBOOT);
-        $this->assertEquals(true, $setRebootStatus);
+        $this->assertTrue($setRebootStatus);
     }
     public function testKernelSystemBootStatus()
     {
         $kernel = new Kernel(\App\SmallBlogSystem\Level\KernelStartInfo::BOOT);
         $setBootStatus = $kernel->changeSystemStatus(\App\SmallBlogSystem\Level\KernelStartInfo::BOOT);
-        $this->assertEquals(true, $setBootStatus);
+        $this->assertTrue($setBootStatus);
     }
 }
