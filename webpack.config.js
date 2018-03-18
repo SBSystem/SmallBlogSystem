de35d1426d4ceac23c7d043c98b82b9652248a0b
Accolade <sorychtak@gmail.com>
2018-03-18 15:33:16 +0100

    Add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,43 @@
 var Encore = require('@symfony/webpack-encore');
 
+/**
+ * Define the first configuration
+ * Admin
+ */
 Encore
     // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+
+    // read js/admin.js -> output public/build/js/admin.js etc.
+    .addEntry('js/admin', './assets/js/admin.js')
+    .addStyleEntry('css/admin', './assets/scss/admin/admin.scss')
+
+    .addEntry('js/front', './assets/js/front.js')
+    .addStyleEntry('css/front', './assets/scss/front/front.scss')
+
+    .addStyleEntry('css/shared', './assets/scss/shared/shared.scss')  
+
+    //.addStyleEntry('css/shared', './assets/scss/shared/shared.scss')    
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // enable autoprefixer
+    .enablePostCssLoader()
+
+    // for legacy applications that require $/jQuery as a global variable
+    .autoProvidejQuery()
+
     .cleanupOutputBeforeBuild()
+
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
 ;
+
 
 module.exports = Encore.getWebpackConfig();
