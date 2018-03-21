f283f50a807a125942427523b774b96d5fc8a9ef
Mariusz08 <mariuszhg08@gmail.com>
2018-03-21 19:55:17 +0100

    Added login controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,10 +8,16 @@ use App\SmallBlogSystem\Level\LogLevel;
 use App\SmallBlogSystem\Managers\LogManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use SmallBlogSystem\TemplatesReturn\TwigTemplatesConfig;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
-    public function indexPage()
+    public function indexPage(): Response
     {
         $kernel = new Kernel(KernelStartInfo::BOOT);
         $logger = new LogManager();
@@ -26,4 +32,62 @@ class IndexController extends Controller
             'generator' => TwigTemplatesConfig::GENERATOR
         ));
     }
+    public function loginPage(Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('login', TextType::class, array('label' => 'Login', 'placeholder' => 'Login...'))
+            ->add('password', PasswordType::class, array('label' => 'Haslo', 'placeholder' => 'Haslo...'))
+            ->add('submit', SubmitType::class, array('label' => 'Zaloguj się!'))
+            ->getForm();
+
+        return $this->render('Index/login.html.twig', array(
+            'title' => TwigTemplatesConfig::TITLE,
+            'charset' => TwigTemplatesConfig::CHARSET,
+            'pageAuthor' => TwigTemplatesConfig::PAGE_AUTHOR,
+            'generator' => TwigTemplatesConfig::GENERATOR,
+            'form' => $form->createView()
+        ));
+    }
+    public function remindPassword(): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', TextType::class, array('label' => 'E-mail', 'placeholder' => 'E-mail...'))
+            ->add('submit', SubmitType::class, array('label' => 'Wyslij wiadomosc'))
+            ->getForm();
+
+        return $this->render('Index/login.html.twig', array(
+            'title' => TwigTemplatesConfig::TITLE,
+            'charset' => TwigTemplatesConfig::CHARSET,
+            'pageAuthor' => TwigTemplatesConfig::PAGE_AUTHOR,
+            'generator' => TwigTemplatesConfig::GENERATOR,
+            'form' => $form->createView()
+        ));
+    }
+    public function logout(): Response
+    {
+        $this->redirectToRoute('/');
+    }
+
+    public function loginFormPost($_POST)
+    {
+
+    }
+    public function loginUser()
+    {
+        $userRepo = $this->getDoctrine()->getRepository('User');
+        $entity = $userRepo->find(1);
+        $authenticate = new UsernamePasswordToken(
+            $entity,
+            null,
+            'user_db',
+            $entity->getRole()
+        );
+
+        $this->container->get('security.context')->setToken($authenticate);
+    }
+    public function logoutUser()
+    {
+
+    }
+
 }
